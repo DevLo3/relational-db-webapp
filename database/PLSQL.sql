01d8e87ced4196877396bb91be935a3bb8f617cf
DevLo3 <julio.diaz3rd@gmail.com>
2025-05-21 16:50:55 -0700

    create PLSQL.sql file and add CREATE procedure

---
+++ database/PLSQL.sql
@@ -0,0 +1,42 @@
+-- #############################
+-- CREATE cust_orders
+-- #############################
+DROP PROCEDURE IF EXISTS sp_CreateCustOrder;
+
+DELIMITER //
+CREATE PROCEDURE sp_CreateCustOrder(
+    IN p_customer VARCHAR(50), 
+    IN p_product VARCHAR(50), 
+    IN p_price DECIMAL(10,2), 
+    IN p_quantity INT,
+    IN p_delivery TINYINT(1),
+    IN p_recurring TINYINT(1),
+    OUT p_id INT)
+BEGIN
+    INSERT INTO Cust_Prod_Orders (
+    customer_id, 
+    product_id, 
+    price, 
+    quantity, 
+    needs_delivery, 
+    recurring
+    ) 
+    VALUES (
+    p_customer,
+    p_product,
+    p_price, 
+    p_quantity, 
+    p_delivery, 
+    p_recurring
+    );
+
+    -- Store the ID of the last inserted row
+    SELECT LAST_INSERT_ID() into p_id;
+    -- Display the ID of the last inserted customer order.
+    SELECT LAST_INSERT_ID() AS 'new_id';
+
+    -- Example of how to get the ID of the newly created person:
+        -- CALL sp_CreatePerson('Theresa', 'Evans', 2, 48, @new_id);
+        -- SELECT @new_id AS 'New Person ID';
+END //
+DELIMITER ;
