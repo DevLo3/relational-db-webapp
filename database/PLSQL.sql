0288064646ac887f1b77737163b9744062353d3e
Julio DiazIii <diazj2@classwork.engr.oregonstate.edu>
2025-05-21 20:46:37 -0700

    add UPDATE stored procedure

--- database/PLSQL.sql
+++ database/PLSQL.sql
@@ -1,3 +1,14 @@
+/*
+****
+
+Citations:
+    
+Leveraged starter code from Web Application Technology Canvas exploration
+Link: https://canvas.oregonstate.edu/courses/1999601/pages/exploration-web-application-technology-2?module_item_id=25352948
+
+****
+*/
+
 -- #############################
 -- CREATE cust_orders
 -- #############################
@@ -11,7 +22,8 @@ CREATE PROCEDURE sp_CreateCustOrder(
     IN p_quantity INT,
     IN p_delivery TINYINT(1),
     IN p_recurring TINYINT(1),
-    OUT p_id INT)
+    OUT p_id INT
+    )
 BEGIN
     INSERT INTO Cust_Prod_Orders (
     customer_id, 
@@ -35,5 +47,34 @@ BEGIN
     -- Display the ID of the last inserted customer order.
     SELECT LAST_INSERT_ID() AS 'new_id';
 
+END //
+DELIMITER ;
+
+-- #############################
+-- UPDATE cust_orders
+-- #############################
+DROP PROCEDURE IF EXISTS sp_UpdateCustOrder;
+
+DELIMITER //
+CREATE PROCEDURE sp_UpdateCustOrder(
+    IN p_id INT, 
+    IN p_customer INT, 
+    IN p_product INT, 
+    IN p_price DECIMAL(10,2), 
+    IN p_quantity INT,
+    IN p_delivery TINYINT(1),
+    IN p_recurring TINYINT(1)
+    )
+
+BEGIN
+    UPDATE Cust_Prod_Orders
+    SET
+        customer_id = p_customer,
+        product_id = p_product,
+        price = p_price, 
+        quantity = p_quantity, 
+        needs_delivery = p_delivery, 
+        recurring = p_recurring
+    WHERE cust_prod_order_id = p_id
 END //
 DELIMITER ;
